ce4e04abc001fa111ecede29a044caa909a9b62c
SabinaSolt <sabina.soltobaeva@gmail.com>
2020-04-15 11:37:06 +0200

    exo-1 ok ok

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,8 +1,13 @@
 console.log('exercice 1');
 //jQuery est un framework qui permet d'écrire le code plus concis comparé au JavaScript.
-// La différence consiste aussi au fait que JavaScript manipule les noeuds du DOM,
+// La différence consiste aussi dans le fait que JavaScript manipule les noeuds du DOM,
 // alors que jQuery manipule les collections d'objets construites autour de ces noeuds.
 
+//ces lignes permettent de lancer le script jQuery une fois que la page est chargée.
+//Du coup, l'emplacement de la balise <script> à l'intérieur du document HTML n'a plus d'incidence
+// sur l'éxecution du script
+
+//$ est le symbole qui permet de repérer l'utilisation de jQuery
 $(document).ready(function(){
     console.log('Je peux maintenant écrire du code jQuery');
 });
