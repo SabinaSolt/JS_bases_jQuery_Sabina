806c27f98f4e98db4dd001e6842facdc1a242301
SabinaSolt <sabina.soltobaeva@gmail.com>
2020-04-15 12:02:12 +0200

    exo-2 ok

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,22 @@
 console.log('exercice 2');
 
+// Sélectionner le titre du blog : "John's Blog".
+$("#title");
+//     Sélectionner tous les éléments du sidemenu de gauche.
+$("a");
+//     Sélectionner le footer.
+$("footer");
+//     Sélectionner l'élément qui permet de faire la barre de recherche.
+$(".input-group");
+// Sélectionner tous les glyphicons de la page.
+$(".glyphicon");
+//     Sélectionner tous les labels verts.
+$(".label-success");
+//     Sélectionner tous les paragraphes qui ont comme classe 'intro'.
+$("p .intro");
+//     Sélectionner toutes les images avec toutes les icones dont la classe est 'glyphicon-time'
+$("img .glyphicon-time");
+// Sélectionner le premier élément du sidemenu.
+$(".nav-pills:first-child");
+//     Bonus : sélectionner un paragraphe sur deux.
+$("p:nth-of-type(2n)");
