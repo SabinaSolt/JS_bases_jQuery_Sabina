88c4036a7df6ef9213ca095e4e2742125fcf1eba
SabinaSolt <sabina.soltobaeva@gmail.com>
2020-04-15 16:03:34 +0200

    exo-3 ok

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,7 +1,11 @@
 console.log('exercice 1');
-//jQuery est un framework qui permet d'écrire le code plus concis comparé au JavaScript.
+//jQuery est une librairie qui permet d'écrire le code plus concis comparé au JavaScript.
 // La différence consiste aussi dans le fait que JavaScript manipule les noeuds du DOM,
 // alors que jQuery manipule les collections d'objets construites autour de ces noeuds.
+
+//pour utiliser jQuery on mettre la balise pour l'inclure au-dessus de la balise <script src="script.js"></script>
+//Comme le navigateur lit la page de haut en bas, si on mets la balise jQuery après la balise qui refere notre code,
+//qui à son tour utilise jQuery, le navigateur ne saura tout simplement pas comment interpreter notre code.
 
 //ces lignes permettent de lancer le script jQuery une fois que la page est chargée.
 //Du coup, l'emplacement de la balise <script> à l'intérieur du document HTML n'a plus d'incidence
--- exo-3/script.js
+++ exo-3/script.js
@@ -1,1 +1,39 @@
 console.log("exercice 3");
+$(document).ready(function(){
+
+    $(".jumbotron h1").text("Sabina");
+    $("#tagline").html("Une perle rare, j'aime rester dans ma coquille...<br>#ResterChezVous");
+    $("ul:first-child").remove();
+    $(".navbar-right span").removeClass("glyphicon-log-in");
+    $(".navbar-right span").addClass("glyphicon-user");
+    $("footer p").text("Copyright 2017");
+    $("footer p").css("font-weight","bold");
+    //etape 6
+    let $paragraphs=$("#work1 p");
+    $paragraphs.each(function(index, element) {
+        let $paragraph=$(element);
+        let numProjet=index+1;
+        $paragraph.text("Mon projet "+numProjet);
+    });
+    //etape 7
+
+    $("img").attr("src","https://cuttingsjewellers.co.uk/files/cache/a4d9c40583c50b9a5d3616be2efc5164_f1097.jpg");
+
+    //etape 8
+
+    let newDiv=$("<div>");
+    newDiv.addClass("col-sm-12");
+    $("#work1").prepend(newDiv);
+
+    let newP=$("<p>");
+    $(newP).text("Mon nouveau projet");
+    $(newDiv).append(newP);
+
+    let newImg=$("<img>");
+    $(newImg).attr("src","https://img.etimg.com/thumb/msid-71705293,width-643,imgsize-408826,resizemode-4/basra-pearls.jpg");
+    $(newImg).css("height","250px");
+    $(newDiv).append(newImg);
+
+    //etape 9
+    $("footer").addClass("intro");
+});
