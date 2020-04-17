00c0d9d765c09cfe2db4b6c121926345b8f6ee41
SabinaSolt <sabina.soltobaeva@gmail.com>
2020-04-17 16:45:20 +0200

    exo-8 en cours

--- exo-8/script.js
+++ exo-8/script.js
@@ -1,1 +1,11 @@
 console.log("exercice 8");
+$(document).ready(function () {
+    $("#datepicker").datepicker();
+    // $('#saucisses').accordion({
+    //     collapsible:true,
+    // });
+    $('#tabs').tabs();
+    $('#carousel').slick({
+    });
+
+});
